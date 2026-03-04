9b62a4d7639eb276968e1207202c088b2dd51912
igorvargasp <igorvargasp@hotmail.com>
2026-03-04 00:32:57 -0300

    Add NodeJS server with four routes
    - Home (/) returns HTML 200
    - About (/about) returns HTML 200
    - My Account (/my-account) returns plain text 403
    - Any other URL returns plain text 404

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import http from 'http';
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  const url = req.url;
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Home</h1>');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>About</h1>');
+  } else if (url === '/my-account') {
+    res.writeHead(403, { 'Content-Type': 'text/plain' });
+    res.end('You have no access to this page');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
